f4d8bae68af64cb338a20aef46e4f2ee23db907e
showkatbinidris70 <showkatbinidris70@gmail.com>
2023-11-14 17:03:16 +0600

    typing animation added

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,21 +8,23 @@ export default function Home() {
         <Fade bottom>
           <div className="row">
             <div className="col-sm-6 col-md-4">
-              <div className="card shadow-sm">
+              <div className="card shadow-sm h-100">
                 <h1 className="text-success">Hello 1</h1>
-                <h1>Hello</h1>
+                <div id="fragment1">
+                  <h1>Hariharan</h1>
+                </div>
                 <h1>Hello</h1>
               </div>
             </div>
             <div className="col-sm-6 col-md-4">
-              <div className="card shadow-sm">
+              <div className="card shadow-sm h-100">
                 <h1>Hello 1</h1>
                 <h1>Hello</h1>
                 <h1>Hello</h1>
               </div>
             </div>
             <div className="col-sm-6 col-md-4">
-              <div className="card shadow-sm">
+              <div className="card shadow-sm h-100">
                 <h1>Hello 1</h1>
                 <h1>Hello</h1>
                 <h1>Hello</h1>
